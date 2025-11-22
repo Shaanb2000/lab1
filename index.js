204608c25687fc6c76f303120fef1b298e591c29
Shaanb2000 <s4bhakta@ucsd.edu>
2025-11-21 23:19:52 -0800

    Move projects.json to projects folder and update related files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
-const allProjects = await fetchJSON('/lab1/lib/projects.json');
+const allProjects = await fetchJSON('./projects/projects.json');
+
 
 
 const latest = allProjects.slice(0, 3);
--- projects/projects.js
+++ projects/projects.js
@@ -1,12 +1,12 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
-const projects = await fetchJSON('/lab1/lib/projects.json');
-
+// load JSON from SAME folder
+const projects = await fetchJSON('./projects.json');
 
 const container = document.querySelector('.projects');
 renderProjects(projects, container, 'h2');
 
 const titleEl = document.querySelector('.projects-title');
 if (titleEl) {
-    titleEl.textContent = `${projects.length} Projects`;
+  titleEl.textContent = `${projects.length} Projects`;
 }
